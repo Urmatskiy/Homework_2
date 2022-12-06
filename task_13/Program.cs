0b158677e342674c443932a8062a24b6a8d9ff9b
Urmatskiy <nickiton14@gmail.com>
2022-12-06 18:37:13 +0300

    Выполнил 13 задание

---
+++ task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int var = int.Parse(Console.ReadLine());
+int second = var;
+
+if (var < 100) Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (second > 999)
+    {
+        second = second / 10;
+    }
+    Console.Write($"Третьей цифрой числа {var} является цифра {second % 10}");
+}
