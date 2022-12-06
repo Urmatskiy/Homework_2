5f7b9846c58621074910ab8a120b3bc4be89abad
Urmatskiy <nickiton14@gmail.com>
2022-12-06 18:42:37 +0300

    Выполнил 15 задание

---
+++ task_15/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day > 5) Console.WriteLine("Это выходной");
+else Console.WriteLine("Это рабочий день :(");
