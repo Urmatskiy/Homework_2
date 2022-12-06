297f8c12327223827dad7cc4f11b333e439012c7
Urmatskiy <nickiton14@gmail.com>
2022-12-06 18:22:43 +0300

    Выполнил десятое задание

---
+++ task_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число: ");
+int var = int.Parse(Console.ReadLine());
+int second = var / 10;
+int result = second % 10;
+Console.Write($"{result} является второй цифрой числа {var}");
